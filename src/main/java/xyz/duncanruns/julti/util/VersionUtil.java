3a342c7f7bf53af642d2eb93124491ea16d5be00
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-06-16 19:47:48 -0300

    downstream from jinglestream

--- src/main/java/xyz/duncanruns/julti/util/VersionUtil.java
+++ src/main/java/xyz/duncanruns/julti/util/VersionUtil.java
@@ -47,11 +47,6 @@ public final class VersionUtil {
             if (version == null) {
                 throw new IllegalArgumentException("Version can not be null");
             }
-            // hack for pre-releases & release candidates
-            int idx = version.indexOf('-');
-            if (idx != -1) {
-                version = version.substring(0, idx);
-            }
             if (!version.matches("[0-9]+(\\.[0-9]+)*")) {
                 throw new IllegalArgumentException("Invalid version format");
             }
